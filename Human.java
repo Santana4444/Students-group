fdf12ab43d3e1ad58864b7a8186301dc8a15c985
Santana4444 <94118302+Santana4444@users.noreply.github.com>
2021-11-11 14:55:51 +0200

    Create Human.java

---
+++ Human.java
@@ -0,0 +1,59 @@
+package sample;
+
+public class Human {
+	
+	private String lastName;
+	private String firstName;
+	private int age;
+	private Gender gender;
+	
+	public Human(String lastName, String firstName, int age, Gender gender) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public Human() {
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Personal Information: lastName -" + lastName + 
+				", firstName -" + firstName +
+				", age -" + age + "year old, gender -" + gender + ".";
+	}
+		
+}
